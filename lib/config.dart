0cb445880a6104a4db7351f35e1e6f3cafc9deae
Vedansh-14 <vedansh@melooha.com>
2023-09-18 17:42:03 +0530

    Miscellaneous

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,3 @@
 class AppConfig {
-  static const String githubAuthToken = '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';
+  static const String githubAuthToken = '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';
 }
